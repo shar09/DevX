b2d55c710fbfaf4461901b77c5ff45327d570222
shar09 <sharathtelu9@gmail.com>
2020-09-22 16:33:07 -0400

    alert reducer added

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,20 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
     return (
+        <Provider store={store}>
             <Router>
                 <Fragment> 
                     <Navbar />
                     <Route exact path = "/" component= {Landing} />
                     <section className="container">
+                        <Alert />
                         <Switch>
                             <Route exact path="/register" component ={Register} />
                             <Route exact path="/login" component = {Login} />
@@ -20,6 +26,7 @@ const App = () => {
                     </section>
                 </Fragment>
             </Router>
+        </Provider>    
     );
 };
 
