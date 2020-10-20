be810b5b90706dcb28f9bd2d2928c7113cd24db1
shar09 <sharathtelu9@gmail.com>
2020-10-20 11:10:33 -0400

    prepared for deployment

--- server.js
+++ server.js
@@ -9,14 +9,22 @@ connectDb();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profiles'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-const PORT = process.env.port || 5000;
+// Serve static assets in production
+if(process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        req.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
