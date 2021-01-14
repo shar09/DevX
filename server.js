154dd414c5d451ba489bd068de2e34ede8dbc954
shar09 <sharathtelu9@gmail.com>
2021-01-14 16:18:48 -0600

    heroku route fixed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDb = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -16,14 +17,14 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets in production
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
-
+  
     app.get('*', (req, res) => {
-        req.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
-    })
-}
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 5000;
 
